86c32bf55d38bb10044c04668a470d1e30adf730
vivianlee03 <vivian@u.sogang.ac.kr>
2023-09-17 22:16:53 +0900

    Add basic main page with search component

--- src/pages/Search/Search.js
+++ src/pages/Search/Search.js
@@ -1,7 +1,116 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
+import { ReactComponent as Logo } from "./Logo.svg";
+import Flex from "../../components/atoms/Flex";
+import { styled } from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import SearchResults from "./SearchResults";
 
 const Search = () => {
-  return <div>search</div>;
+  const [searchFocused, setSearchFocused] = useState(false);
+  const [searchText, setSearchText] = useState("");
+  const searchBoxRef = useRef(null);
+
+  // Function to handle outside click
+  const handleClickOutside = (e) => {
+    if (searchBoxRef.current && !searchBoxRef.current.contains(e.target)) {
+      setSearchFocused(false);
+    }
+  };
+
+  // Add click event listener to the document
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  // Function to handle search input click
+  const handleSearchInputClick = () => {
+    setSearchFocused(true);
+  };
+
+  // Function to handle input change
+  const handleInputChange = (e) => {
+    setSearchText(e.target.value);
+  };
+
+  return (
+    <Flex>
+      <div style={{ marginTop: "220px" }} />
+      <Logo width={280} height={49} />
+      <SearchContainer focused={searchFocused}>
+        <SearchBox ref={searchBoxRef} focused={searchFocused}>
+          <div style={{ display: "flex" }}>
+            <FontAwesomeIcon
+              icon={faMagnifyingGlass}
+              style={{
+                fontSize: "20px",
+                color: "#8C8C8C",
+                marginLeft: "20px",
+                marginTop: "15px",
+              }}
+            />
+            <SearchInput
+              onClick={handleSearchInputClick}
+              value={searchText}
+              onChange={handleInputChange}
+              placeholder="종목 명, 애널리스트 명을 검색해 보세요."
+            />
+          </div>
+          {searchFocused ? (
+            <SearchResultsContainer>
+              <SearchResults />
+            </SearchResultsContainer>
+          ) : null}
+        </SearchBox>
+      </SearchContainer>
+    </Flex>
+  );
 };
+
+const SearchContainer = styled.div`
+  position: relative;
+  width: 500px;
+  height: ${(props) => (props.focused ? "auto" : "50px")};
+  max-height: ${(props) => (props.focused ? "400px" : "50px")};
+  margin-top: 50px;
+  transition: height 0.3s ease-in-out;
+`;
+
+const SearchBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  border: 1px solid #d5d8dc; /* Add border */
+  border-radius: 10px;
+  color: ${(props) => (props.focused ? "#000" : "#8c8c8c")};
+  display: flex;
+  justify-content: left;
+  box-shadow: ${(props) =>
+    props.focused ? "2px 2px 20px -5px rgba(0, 0, 0, 0.25)" : "none"};
+`;
+
+const SearchInput = styled.input`
+  width: 100%;
+  height: 50px;
+  margin-left: 15px;
+  color: ${(props) => (props.value ? "#000" : "#8c8c8c")};
+  font-family: "Pretendard";
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 19px;
+  border: none;
+  outline: none;
+  background: transparent;
+`;
+
+const SearchResultsContainer = styled.div`
+  max-height: 350px; /* Set a max height for SearchResultsContainer */
+  overflow-y: auto; /* Add scrollbar when content exceeds max height */
+`;
 
 export default Search;
---
+++ src/pages/Search/SearchResults.js
@@ -0,0 +1,130 @@
+import React from "react";
+import { styled } from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChartLine, faUser } from "@fortawesome/free-solid-svg-icons";
+
+const SearchResults = () => {
+  return (
+    <SearchResultsDiv>
+      <Title>
+        <FontAwesomeIcon
+          icon={faChartLine}
+          style={{
+            fontSize: "16px",
+            color: "#717FFE",
+          }}
+        />
+        종목
+      </Title>
+      <Result>
+        <ResultContents>
+          <ResultLeft>삼성전자</ResultLeft>
+          <ResultRight>005930</ResultRight>
+        </ResultContents>
+      </Result>
+      <Result>
+        <ResultContents>
+          <ResultLeft>삼성전자</ResultLeft>
+          <ResultRight>005930</ResultRight>
+        </ResultContents>
+      </Result>
+      <Title>
+        <FontAwesomeIcon
+          icon={faUser}
+          style={{
+            fontSize: "16px",
+            color: "#717FFE",
+          }}
+        />
+        애널리스트
+      </Title>
+      <Result>
+        <ResultContents>
+          <ResultLeft>김삼성</ResultLeft>
+          <ResultRight>미래투자증권</ResultRight>
+        </ResultContents>
+      </Result>
+      <Result>
+        <ResultContents>
+          <ResultLeft>김삼성</ResultLeft>
+          <ResultRight>미래투자증권</ResultRight>
+        </ResultContents>
+      </Result>
+      <Result>
+        <ResultContents>
+          <ResultLeft>김삼성</ResultLeft>
+          <ResultRight>미래투자증권</ResultRight>
+        </ResultContents>
+      </Result>
+    </SearchResultsDiv>
+  );
+};
+
+const SearchResultsDiv = styled.div`
+  display: flex;
+  justify-content: left;
+  flex-direction: column;
+`;
+
+const Title = styled.div`
+  display: flex;
+  align-items: flex-start;
+  padding: 7px 9px;
+  width: fit-content;
+  gap: 10px;
+  border-radius: 5px;
+  background: rgba(113, 127, 254, 0.1);
+  color: #717ffe;
+  font-family: "Pretendard";
+  font-size: 15px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+  margin: 10px 16px;
+  justify-content: flex-start; /* For other browsers */
+  -webkit-box-pack: start; /* For Safari */
+`;
+
+const Result = styled.div`
+  display: flex;
+  padding: 0px 0px 8px 0px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 8px;
+`;
+
+const ResultContents = styled.div`
+  display: flex;
+  width: 450px;
+  height: 26px;
+  justify-content: space-between;
+  align-items: center;
+  padding: 12px 8px;
+  border-radius: 5px;
+
+  &:hover {
+    background-color: #f5f7f9;
+  }
+`;
+
+const ResultLeft = styled.div`
+  color: #000;
+  font-family: "Pretendard";
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 19px;
+  text-align: right;
+`;
+
+const ResultRight = styled.div`
+  color: #8c8c8c;
+  font-family: "Pretendard";
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 19px;
+  text-align: left;
+`;
+export default SearchResults;
